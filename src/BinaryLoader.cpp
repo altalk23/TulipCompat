c16ecae1091ec936eac6cc453a55b6e752969bb5
altalk23 <45172705+altalk23@users.noreply.github.com>
2023-01-29 14:28:27 +0300

    generalize trackers

--- src/BinaryLoader.cpp
+++ src/BinaryLoader.cpp
@@ -20,62 +20,80 @@ public:
 	~Impl();
 
 	ghc::filesystem::path m_path;
-};
 
-namespace {
-	void load(std::string const& path) {
-		// making it lazy since this is a compat thing
+	void loadAndTrack(std::string const& path, std::vector<MappingTracker*> const& trackers) {
+#ifdef GEODE_IS_MACOS
 		if (path.find(".dylib") != std::string::npos) {
 			using namespace std::chrono_literals;
 
-#ifdef GEODE_IS_MACOS
 			auto binary = dlopen(path.c_str(), RTLD_NOW);
 			std::this_thread::sleep_for(100ms); // im not shameful
+		}
 #elif GEODE_IS_WINDOWS
+		if (path.find(".dll") != std::string::npos) {
+			using namespace std::chrono_literals;
+
 			auto test = LoadLibraryA(path.c_str());
 			std::this_thread::sleep_for(5000ms); // im not shameful
+		}
+
 #endif
+
+		for (auto tracker : trackers) {
+			tracker->finalize();
+
+			auto diffs = tracker->getDifferences();
+			log::info("Found {} differences", diffs.size());
+
+			for (auto& [address, size, original, modified] : diffs) {
+				log::info("Has difference at address {} size {}", address, size);
+			}
+
+			auto hookDiffs = tracker->getHookDifferences();
+			log::info("Found {} hook differences", hookDiffs.size());
+
+			for (auto& [hook, metadata, diff] : hookDiffs) {
+				log::info("Has hook at address {}", hook);
+
+				auto res = Mod::get()->patch(diff.m_address, diff.m_original);
+				if (!res) {
+					log::warn("Reverting the hook {} failed", diff.m_address);
+				}
+
+				auto res2 = Mod::get()->addHook(Hook::create(
+				    Mod::get(), diff.m_address, hook, "", metadata, tulip::hook::HookMetadata()
+				));
+			}
 		}
 	}
-}
+};
 
 BinaryLoader::Impl::Impl(ghc::filesystem::path const& path) :
     m_path(path) {
 	for (auto const& entry : ghc::filesystem::directory_iterator(path)) {
 		log::info("Loading {}", entry.path().string());
+
 #ifdef GEODE_IS_MACOS
 		auto gdMap = MappingTracker(reinterpret_cast<void*>(base::get() + 0x1980), 0x4883b2);
+
+		auto loading = std::thread(
+		    &Impl::loadAndTrack, this, entry.path().string(), std::vector<MappingTracker*>({ &gdMap })
+		);
 #elif GEODE_IS_WINDOWS
 		auto gdMap = MappingTracker(reinterpret_cast<void*>(base::get() + 0x1000), 0x280e00);
+		auto cocosMap = MappingTracker(reinterpret_cast<void*>(base::get() + 0x1000), 0x11b400);
+		auto cocosExtensionMap =
+		    MappingTracker(reinterpret_cast<void*>(base::get() + 0x1000), 0x24600);
+
+		auto loading = std::thread(
+		    &Impl::loadAndTrack,
+		    this,
+		    entry.path().string(),
+		    std::vector<MappingTracker*>({ &gdMap, &cocosMap, &cocosExtensionMap })
+		);
 #endif
 
-		auto loading = std::thread(&load, entry.path().string());
 		loading.join();
-
-		gdMap.finalize();
-
-		auto diffs = gdMap.getDifferences();
-		log::info("Found {} differences", diffs.size());
-
-		for (auto& [address, size, original, modified] : diffs) {
-			log::info("Has difference at address {} size {}", address, size);
-		}
-
-		auto hookDiffs = gdMap.getHookDifferences();
-		log::info("Found {} hook differences", hookDiffs.size());
-
-		for (auto& [hook, metadata, diff] : hookDiffs) {
-			log::info("Has hook at address {}", hook);
-
-			auto res = Mod::get()->patch(diff.m_address, diff.m_original);
-			if (!res) {
-				log::warn("Reverting the hook {} failed", diff.m_address);
-			}
-
-			auto res2 = Mod::get()->addHook(Hook::create(
-			    Mod::get(), diff.m_address, hook, "", metadata, tulip::hook::HookMetadata()
-			));
-		}
 	}
 }
 
